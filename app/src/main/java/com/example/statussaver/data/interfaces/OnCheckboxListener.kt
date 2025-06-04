cbfc5fa075b69b827666e27cfd6f16f83817c878
Harikrushn Moradiya <66943772+harimoradiya@users.noreply.github.com>
2025-06-04 22:20:17 +0530

    Delete app directory

--- app/src/main/java/com/example/statussaver/data/interfaces/OnCheckboxListener.kt
+++
@@ -1,8 +0,0 @@
-package com.example.statussaver.data.interfaces
-
-import android.view.View
-import com.example.statussaver.data.model.StatusDataModel
-
-interface OnCheckboxListener {
-    fun onCheckboxListener(view: View?, list: List<StatusDataModel?>?)
-}
--- app/src/main/java/com/example/statussaver/data/interfaces/OnDeleteListener.kt
+++
@@ -1,5 +0,0 @@
-package com.example.statussaver.data.interfaces
-
-interface OnDeleteListener {
-    fun getClick(position: Int)
-}
