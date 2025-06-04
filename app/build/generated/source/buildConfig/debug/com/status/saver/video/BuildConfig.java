3a0d90e7e28bf8c6a5d9045c83f20c988ce78890
harimoradiya <harimoradiya123@gmail.com>
2025-06-04 22:40:41 +0530

    project added

---
+++ app/build/generated/source/buildConfig/debug/com/status/saver/video/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.status.saver.video;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.status.saver.video";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+}
